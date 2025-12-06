f9d1855a1698ad8df6325b7fad1bd7e28ed4b61c
Thakor Kiran <kirannthakor38@gmail.com>
2025-12-06 14:30:09 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -140,6 +140,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "technical.team0004@gmail.com")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "ueqk gmve casx enuq")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
